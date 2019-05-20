e55c35f13ba3392164364b3f31ac9cfb0d2749c4
mmeyer95 <meredith_meyer@berkeley.edu>
2019-05-20 21:24:00 +0000

    Car maintains smooth motion around track, does not avoid collisions.

--- src/main.cpp
+++ src/main.cpp
@@ -92,61 +92,88 @@ int main() {
           auto sensor_fusion = j[1]["sensor_fusion"];
 
           json msgJson;
-
+          
+          //////**TRAJECTORY GENERATION**/////
+		  //Vectors to sent to the simulator as next moves for the car
           vector<double> next_x_vals;
           vector<double> next_y_vals;
-		  //initialize for a straight-ahead move
-          //float max_S_change = 0.4;
-          if (end_path_s!=0 && end_path_d!=0){
-            double d = end_path_d;
-            double s = end_path_s;
-          }
-          else {
-            double d = car_d;
-          	double s = car_s;
-          }
+          //Initialize constants
+          double safety = 1.1;
+          double max_speed = 22.352; //maximum speed in m/s (50 MPH)
+          double time_step = 0.02; //seconds
+          double dist_step = max_speed*time_step/safety; //max change in distance allowed between each point
+          vector<double> lane_mults = {2,6,10}; //multipliers of d vector to be in the center of lanes 0, 1, 2 resp.
+          int current_lane;	//the lane the car is currently in (1, 2, or 3)
+
+          //Collect values for spline
           vector<double> spline_x;
           vector<double> spline_y;
-          vector<double> spline_time;
-          //point at the end of last move
-          vector<double> XY = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          spline_x.push_back(XY[0]);
-          spline_y.push_back(XY[1]);
-          spline_time.push_back(0);
-          //point 1 down the road
-          s = s+1;
-          XY = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          spline_x.push_back(XY[0]);
-          spline_y.push_back(XY[1]);
-          spline_time.push_back(0.5);
-          //point 2 down the road
-          s = s+1;
-          XY = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          spline_x.push_back(XY[0]);
-          spline_y.push_back(XY[1]);
-          spline_time.push_back(1);
-          //find next points in s & d for straight-away, convert to x*y, add to spline arrays
-          //for (int i=0; i<10; i++){
-          //s = s + max_S_change;
-          //vector<double> XY = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-          //std::cout << "s=" << s <<std::endl;
-          //std::cout << "d=" << d <<std::endl;
-          //std::cout << "X=" << XY[0] <<std::endl;
-          //std::cout << "Y=" << XY[1] <<std::endl;
-          //spline_x.push_back(XY[0]);
-          //spline_y.push_back(XY[1]);
-          //}
-          //compute the spline and fill next x & y based on calculated positions at 0.2 s intervals
-          tk::spline s_x;
-          s_x.set_points(spline_time,spline_x);
-          tk::spline s_y;
-          s_y.set_points(spline_time, spline_y);
-          for (int i=1; i<11; i++){
-            next_x_vals.push_back(s_x(0.2*i));
-            //std::cout << "next_x: " << s_x(0.2*i) << std::endl;
-            next_y_vals.push_back(s_y(0.2*i));
-            //std::cout << "next_y: " << s_y(0.2*i) << std::endl;
+          //Count number of points left
+          int points_left = previous_path_x.size();
+          std::cout << "Points left: " << points_left << std::endl;
+          //Add un-processed points to path to send to simulator
+          if (points_left>0){
+          	for (int i=0; i<points_left;i++){
+          		next_x_vals.push_back(previous_path_x[i]);
+          		next_y_vals.push_back(previous_path_y[i]);
+          	}
+          }  
+          //Use previous points for smoothness in spline calc
+          if (points_left<2) {
+    	  	//Use 1 points straight behind and current position if <2 un-processed points
+    		double prev_car_x = car_x - cos(car_yaw);
+    		double prev_car_y = car_y - sin(car_yaw);
+
+    		spline_x.push_back(prev_car_x);
+    		spline_x.push_back(car_x);
+
+    		spline_y.push_back(prev_car_y);
+    		spline_y.push_back(car_y);
+		  }
+		  else {
+            for (int i=0; i<points_left;i++){
+              spline_x.push_back(previous_path_x[i]);
+          	  spline_y.push_back(previous_path_y[i]);
+            }
           }
+          //Use points down the road to calculate spline
+          vector<double> wp1 = getXY(car_s + 25, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> wp2 = getXY(car_s + 50, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          //double wp1_x = map_waypoints_x[wp1] + map_waypoints_dx[wp1]*lane_mults[current_lane];
+          //double wp1_y = map_waypoints_y[wp1] + map_waypoints_dy[wp1]*lane_mults[current_lane];
+          //double wp2_x = wp2[0];
+          //double wp2_y = wp2[1];
+          spline_x.push_back(wp1[0]);
+          spline_x.push_back(wp2[0]);
+          spline_y.push_back(wp1[1]);
+          spline_y.push_back(wp2[1]);
+          for (int i=0; i<spline_x.size();i++){
+          	std::cout << "spline x: "<< spline_x[i] <<std::endl;
+          	std::cout << "spline y: "<< spline_y[i] <<std::endl;
+          }
+          //Compute the spline and fill next x & y based on calculated positions at 0.02 s intervals
+          tk::spline path;
+          path.set_points(spline_x,spline_y);
+
+          //Get 1 second of path data, including any un-processed points from last trajectory
+          double last_x;
+          if (points_left !=0){
+            last_x = next_x_vals.back(); //last x point in trajectory
+          }
+          else {last_x = car_x;}
+          double dist_to = distance(wp1[0], wp1[1], car_x, car_y); //distance to next reference
+          double n_steps = dist_to/dist_step; //# steps to get to target at desired speed
+          double x_step = (wp1[0]-car_x)/n_steps;
+          for (int i=0; i<50-points_left; i++){
+            //find the x value corresponding to s value of allowable speed
+            last_x += x_step;
+            next_x_vals.push_back(last_x);
+            std::cout << "next_x: " << last_x << std::endl;
+            next_y_vals.push_back(path(last_x));
+            std::cout << "next_y: " << path(last_x) << std::endl;
+          }
+          
+          
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
